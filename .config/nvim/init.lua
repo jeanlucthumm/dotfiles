8dc3f88a0903dd1a18b5f983c1ae2b399e584602
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2021-08-15 18:10:08 -0700

    nvim: add initial lua config file

---
+++ .config/nvim/init.lua
@@ -0,0 +1,265 @@
+---- Aliases
+local opt = vim.opt
+local g = vim.g
+local api = vim.api
+local env = vim.env
+local fn = vim.fn
+
+---- Plugins
+require "paq" {
+    "savq/paq-nvim",
+    "sheerun/vim-polyglot",
+
+    -- LSP & DAP
+    "neovim/nvim-lspconfig",
+    "kabouzeid/nvim-lspinstall",
+    "mfussenegger/nvim-dap",
+    "rcarriga/nvim-dap-ui",
+    "hrsh7th/nvim-compe",
+
+    {
+        "junegunn/fzf",
+        run = function() vim.api.nvim_call_function("fzf#install", {}) end
+    },
+   "junegunn/fzf.vim",
+    "scrooloose/nerdtree",
+    "tpope/vim-commentary",
+
+    "tpope/vim-fugitive",
+    "tpope/vim-dispatch",
+    "vim-airline/vim-airline",
+    "vim-airline/vim-airline-themes",
+    "jiangmiao/auto-pairs",
+    "neomake/neomake",
+    "907th/vim-auto-save",
+    "airblade/vim-gitgutter",
+    "vim-test/vim-test",
+    "moll/vim-bbye",
+    "andrejlevkovitch/vim-lua-format",
+    "pseewald/vim-anyfold",
+    {"iamcco/markdown-preview.nvim", run = "cd app && yarn install"},
+    "MattesGroeger/vim-bookmarks",
+
+    "jeanlucthumm/vim-solarized8",
+    "morhetz/gruvbox",
+    "ryanoasis/vim-devicons"
+}
+
+---- Plugin configuration
+-- LSP keybindings
+g.mapleader = " "; -- sets <Leader> to <space>
+local function on_attach(client, bufnr)
+    -- Sets up LSP keybindings when LSP attaches to the buffer
+    local function bnmap(lhs, rhs, ...)
+        api.nvim_buf_set_keymap(bufnr, "n", lhs, "<Cmd>lua "..rhs.."<CR>", ...)
+    end
+
+    api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+    -- Mappings
+    local opts = { noremap=true, silent=true }
+    bnmap("gd", "vim.lsp.buf.definition()", opts)
+    bnmap("gr", "vim.lsp.buf.references()", opts)
+    bnmap("K", "vim.lsp.buf.hover()", opts)
+    bnmap("<Leader>r", "vim.lsp.buf.rename()", opts)
+    bnmap("<Leader>ks", "vim.lsp.buf.signature_help()", opts)
+    bnmap("<Leader>kl", "vim.lsp.diagnostic.show_line_diagnostic()", opts)
+    bnmap("<Leader>kp", "vim.lsp.diagnostic.goto_prev()", opts)
+    bnmap("<Leader>kn", "vim.lsp.diagnostic.goto_next()", opts)
+
+    -- Capability specific commands
+    if client.resolved_capabilities.document_highlight then
+      -- Highlight symbol in document on hover. Delay is controlled by |updatetime|
+      api.nvim_exec([[
+      augroup lsp_document_highlight
+      autocmd! * <buffer>
+      autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+      autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+      ]], false)
+    end
+    if client.resolved_capabilities.document_format then
+        bnmap("<Leader>f", "vim.lsp.buf.formatting()", opts)
+    end
+    if client.resolved_capabilities.code_lens then
+        -- CodeLens provides extra actions like "Run Test"
+        -- under lang specific unit tests
+        bnmap("<F11>", "vim.lsp.codelens.run()", opts)
+    end
+end
+
+-- LSP Lua config
+local lua_config = {
+    Lua = {
+    runtime = {
+      -- LuaJIT in the case of Neovim
+      version = 'LuaJIT',
+      path = vim.split(package.path, ';'),
+    },
+    diagnostics = {
+      -- Get the language server to recognize the `vim` global
+      globals = {'vim'},
+    },
+    workspace = {
+      -- Make the server aware of Neovim runtime files
+      library = {
+        [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+        [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+      },
+    },
+  }
+}
+
+-- LSP global setup
+local function setup_lsp_servers()
+    -- Call setup on every installed server
+    require'lspinstall'.setup()
+
+    local servers = require'lspinstall'.installed_servers()
+    for _, server in pairs(servers) do
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities.textDocument.completion.completionItem.snippetSupport = true
+        local config = {
+            capabilities = capabilities,
+            on_attach = on_attach,
+        }
+
+        -- Language specific config
+        if server == "lua" then
+            config.settings = lua_config
+        end
+
+        require"lspconfig"[server].setup(config)
+    end
+end
+setup_lsp_servers()
+require'lspinstall'.post_install_hook = function ()
+    -- Automatically reloads after :LspInstall
+    setup_lsp_servers()
+    vim.cmd("bufdo e") -- triggers FileType autocmd to start server
+end
+
+-- Compe setup
+require"compe".setup {
+  enabled = true,
+  autocomplete = true,
+  source = {
+    path = true,
+    buffer = true,
+    nvim_lsp = true,
+    nvim_lua = true,
+    calc = true,
+    spell = true,
+  }
+}
+
+---- Neovim options
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.number = true
+opt.termguicolors = true
+opt.splitright = true
+opt.hidden = true
+opt.mouse = "a"
+opt.updatetime = 500
+opt.guifont = "Fira_Code_Retina_Nerd_Font_Complete:h11"
+opt.completeopt = "menuone,noselect"
+if vim.fn.has("nvim-0.5.0") == 1 then opt.signcolumn = "number" end
+
+---- Global options
+g.neomake_open_list = 2
+g.auto_save = 0
+g.auto_save_events = {"InsertLeave", "TextChanged", "CursorHold"}
+g.neovide_cursor_animation_length = 0.05
+g.bookmark_no_default_key_mappings = 1
+vim.v["test#strategy"] = "neomake"
+
+---- Keymap
+local function map(mode, lhs, rhs, opts)
+    local options = {noremap = true}
+    if opts then options = vim.tbl_extend("force", options, opts) end
+    api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+local function nmap(lhs, rhs, opts) map("n", lhs, rhs, opts) end
+local function imap(lhs, rhs, opts) map("i", lhs, rhs, opts) end
+
+-- g
+nmap("gt", ":tabe<CR>:term<CR>i")
+-- <Leader>
+nmap("<Leader>q", ":q<CR>")
+-- nmap("<Leader>o", ":CocFzfList outline<CR>")
+-- nmap("<Leader>O", ":CocFzfList symbols<CR>")
+-- nmap("<Leader>d", ":CocFzfList diagnostics<CR>")
+nmap("<Leader>q", ":qall<CR>")
+nmap("<Leader><Leader>", ":write<CR>")
+-- <Leader>v    nvim config
+nmap("<Leader>ve", ":exe 'tabedit' stdpath('config').'/init2.lua'<CR>")
+nmap("<Leader>vs", ":exe 'source' stdpath('config').'/init2.lua'<CR>")
+-- <Leader>c    quickfix
+nmap("<Leader>cl", ":cclose<CR>")
+nmap("<Leader>cc", ":cc<CR>")
+nmap("<Leader>co", ":copen<CR>")
+-- <Leader>b    bookmarks
+nmap("<Leader>bb", ":BookmarkToggle<CR>")
+nmap("<Leader>ba", ":BookmarkAnnotate<CR>")
+nmap("<Leader>bo", ":BookmarkShowAll<CR>")
+-- <C-*>
+nmap("<C-h>", ":tabp<CR>")
+nmap("<C-l>", ":tabn<CR>")
+map("t", "<C-h>", "<C-\\><C-n>:tabp<CR>")
+map("t", "<C-l>", "<C-\\><C-l>:tabn<CR>")
+map("t", "<C-w><C-w>", "<C-\\><C-l><C-w><C-w>")
+nmap("<C-p>", ":Commands<CR>")
+nmap("<C-e>", ":Buffers<CR>")
+nmap("<C-A-e>", ":Files<CR>")
+nmap("<A-1>", ":NERDTreeToggle")
+nmap("<A-f>", ":NERDTreeFind")
+-- <F*>
+nmap("<F4>", ":Bdelete<CR>")
+-- Auto completion
+imap("<Tab>", "pumvisible() ? '<C-n>' : '<Tab>'", {expr = true})
+imap("<S-Tab>", "pumvisible() ? '<C-p>' : '<S-Tab>'", {expr = true})
+local cr_expr =
+    "pumvisible() ? (empty(v:completed_item)?'<C-n>':'<C-g>u<CR>') : " ..
+        "'<C-g>u<CR>'"
+imap("<CR>", cr_expr, {expr = true})
+
+---- Theme
+function SolarizedTheme()
+    g.airline_theme = "solarized"
+    -- Docstrings should be the same color as regular comments
+    vim.cmd("hi! link rustCommentLineDoc Comment")
+    vim.cmd("colorscheme solarized8")
+end
+
+function GruvboxTheme()
+    g.gruvbox_italic = 1
+    g.gruvbox_bold = 1
+    g.airline_thee = "gruvbox"
+    vim.v["$BAT_THEME"] = "gruvbox"
+    vim.cmd("colorscheme gruvbox")
+end
+
+local function fallbackTheme()
+    opt.background = "light"
+    SolarizedTheme()
+end
+
+vim.cmd("hi! link pythonSpaceError Normal")
+
+if env.TERM == "xterm-kitty" then
+    if env.KITTY_THEME == "solarized-light" then
+        opt.background = "light"
+        SolarizedTheme()
+    elseif env.KITTY_THEME == "solarized-dark" then
+        opt.background = "dark"
+        SolarizedTheme()
+    else
+        fallbackTheme()
+    end
+else
+    fallbackTheme()
+end
+
+---- Commands
