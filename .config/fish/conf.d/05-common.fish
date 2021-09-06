6dd590bce710a7e451dd8aa72c18fe99d196a85f
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2021-09-06 11:46:35 +0300

    fish: add common lib

---
+++ .config/fish/conf.d/05-common.fish
@@ -0,0 +1,9 @@
+function generic_install
+  if [ "$OS" = "Linux" -a -f "/etc/arch-release" ]
+    pacman -S $argv
+  else if [ "$OS" = "Darwin" ]
+    brew install $argv
+  else
+    return 1
+  end
+end
