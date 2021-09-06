797edaddb66d6db7db6cf533426800e6a2cdbd81
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2021-09-06 14:04:44 +0300

    fish: generic_install passes --needed to pacman

--- .config/fish/conf.d/05-common.fish
+++ .config/fish/conf.d/05-common.fish
@@ -1,6 +1,6 @@
 function generic_install
   if [ "$OS" = "Linux" -a -f "/etc/arch-release" ]
-    pacman -S $argv
+    pacman -S --needed $argv
   else if [ "$OS" = "Darwin" ]
     brew install $argv
   else
