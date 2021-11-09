194a8d1d6cdfca1b95da53653aa76cb02bbef269
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2021-11-09 15:49:45 -0800

    nvim: bootstrap packer

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,6 +6,19 @@ local env = vim.env
 local fn = vim.fn
 local cmd = vim.cmd
 
+--- Packer
+local packer_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(packer_path)) > 0 then
+    PackerBootstrap = fn.system({
+        'git',
+        'clone',
+        '--depth',
+        '1',
+        'https://github.com/wbthomason/packer.nvim',
+        packer_path
+    })
+end
+
 require'packer'.startup(function(use)
     use 'wbthomason/packer.nvim'
 
@@ -183,6 +196,7 @@ require'packer'.startup(function(use)
     use 'vim-test/vim-test'
     use '907th/vim-auto-save'
 
+    if PackerBootstrap then require('packer').sync() end
 end) -- packer
 
 -- For plugin development. Link plugin dir to dev
