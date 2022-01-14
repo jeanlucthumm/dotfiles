b48f11baae09f415985edda1c97426c09bc4f19d
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2022-01-14 15:36:43 -0800

    nvim: Tab now jumps to luasnip locations as well

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -51,6 +51,7 @@ require'packer'.startup(function(use)
         },
         config = function()
             local cmp = require 'cmp'
+            local luasnip = require 'luasnip'
 
             cmp.setup {
                 snippet = {
@@ -66,7 +67,21 @@ require'packer'.startup(function(use)
                     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(),
                                                 {'i', 'c'}),
                     ['<CR>'] = cmp.mapping.confirm({select = true}),
-                    ['<C-y>'] = cmp.mapping(cmp.mapping.close(), {'i', 'c'})
+                    ['<C-y>'] = cmp.mapping(cmp.mapping.close(), {'i', 'c'}),
+                    ['<Tab>'] = cmp.mapping(function(fallback)
+                        if luasnip.expand_or_jumpable() then
+                            luasnip.expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, {"i", "s"}),
+                    ['<S-Tab>'] = cmp.mapping(function(fallback)
+                        if luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, {"i", "s"})
                 },
                 sources = cmp.config.sources {
                     {name = 'nvim_lsp'}, {name = 'luasnip'}, {name = 'buffer'}
