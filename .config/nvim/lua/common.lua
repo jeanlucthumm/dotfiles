4dccb31301758d8f960091dd488b2a4531b782a5
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2022-05-17 22:13:50 -0700

    nvim: migrate to client.server_capabilities

--- .config/nvim/lua/common.lua
+++ .config/nvim/lua/common.lua
@@ -1,14 +1,12 @@
 local api = vim.api
 
-local lsp_status = require'lsp-status'
+local lsp_status = require 'lsp-status'
 lsp_status.register_progress()
 
 local M = {}
 
 function M.hover()
-    if vim.diagnostic.open_float() == nil then
-        vim.lsp.buf.hover()
-    end
+    if vim.diagnostic.open_float() == nil then vim.lsp.buf.hover() end
 end
 
 function M.on_attach(client, bufnr)
@@ -24,10 +22,7 @@ function M.on_attach(client, bufnr)
     api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
     -- Mappings
-    local opts = {
-        noremap = true,
-        silent = true
-    }
+    local opts = {noremap = true, silent = true}
     bnmap("gd", "vim.lsp.buf.definition()", opts)
     bnmap("K", "vim.lsp.buf.hover()", opts)
     bnmap("<Leader>r", "vim.lsp.buf.rename()", opts)
@@ -38,7 +33,7 @@ function M.on_attach(client, bufnr)
     bnmap("<Leader>wl", "PrintTable(vim.lsp.buf.list_workspace_folders())", opts)
 
     -- Capability specific commands
-    if client.resolved_capabilities.document_highlight then
+    if client.server_capabilities.documentHighlightProvider then
         -- Highlight symbol in document on hover. Delay is controlled by |updatetime|
         api.nvim_exec([[
       augroup lsp_document_highlight
@@ -48,10 +43,10 @@ function M.on_attach(client, bufnr)
       augroup END
       ]], false)
     end
-    if client.resolved_capabilities.document_formatting then
+    if client.server_capabilities.documentFormattingProvider then
         bnmap("<Leader>f", "vim.lsp.buf.formatting()", opts)
     end
-    if client.resolved_capabilities.code_lens then
+    if client.server_capabilities.codeLensProvider then
         -- CodeLens provides extra actions like "Run Test"
         -- under lang specific unit tests
         bnmap("<F11>", "vim.lsp.codelens.run()", opts)
