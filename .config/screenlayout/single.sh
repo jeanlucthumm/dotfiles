8653fe4ac6090b0e210a94fc60e2e11d409ad105
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2023-01-23 16:54:01 -0800

    xorg: update monitor name, but really this shouldn't be hardcoded

--- .config/screenlayout/single.sh
+++ .config/screenlayout/single.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output "DP-2" --mode 3440x1440 --rate 144.00
+xrandr --output "DisplayPort-0" --mode 3440x1440 --rate 144.00
 # xrandr --output HDMI-0 --mode 2560x1440 --rotate left
