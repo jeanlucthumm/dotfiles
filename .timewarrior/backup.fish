a9e807a70d64fbf3d6d01af196bbc4abef2493db
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2023-03-30 14:11:58 -0700

    timew: add script for obscuring work time entries

---
+++ .timewarrior/backup.fish
@@ -0,0 +1,11 @@
+#!/bin/fish
+for file in ~/.timewarrior/data/2*.data
+  echo "Backing up $file..."
+  cat $file | python3 ~/.timewarrior/backup-google.py > ~/.timewarrior/google/(basename $file)
+end
+
+set -l totalCount (cat ~/.timewarrior/google/2*.data | wc -l)
+echo >~/.timewarrior/google/tags.data "\
+{
+  \"google\":{\"count\":$totalCount}
+}"
