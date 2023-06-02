539197a7834b1c986490eeebad958c159d9421c5
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2023-06-02 13:19:38 -0700

    task: update ttogle

--- .config/fish/functions/ttoggle.fish
+++ .config/fish/functions/ttoggle.fish
@@ -1,5 +1,19 @@
 function ttoggle -d "Stop this task and make another one active" -a new_task
+  set -l tmpfile "/tmp/toggle_last_id"
   task +ACTIVE export | jq '.[0].id' | read -l active_id
+  echo "Active task is $active_id"
+
+  set -l new_id $new_task
+  if [ "$new_task" = "" ]
+    if [ -e "$tmpfile" ]
+      cat $tmpfile | read -L new_id
+    else
+      echo "No previous id"
+      return 1
+    end
+  end
+
+  echo "$active_id" > $tmpfile
   task stop $active_id
-  task start $new_task
+  task start $new_id
 end
