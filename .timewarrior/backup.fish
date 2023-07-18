8996b61f7512e3c5019855f2a854e235a595b0f3
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2023-07-18 10:37:40 -0700

    timew: update work script

--- .timewarrior/backup.fish
+++ .timewarrior/backup.fish
@@ -1,6 +1,6 @@
 #!/bin/fish
 set -l DIR ~/.timewarrior
-for file in ~/.timewarrior/data/2*.data
+for file in $DIR/data/2*.data
   echo "Backing up $file..."
   cat $file | python3 ~/.timewarrior/backup-google.py > ~/.timewarrior/google/(basename $file)
 end
@@ -10,3 +10,7 @@ echo >~/.timewarrior/google/tags.data "\
 {
   \"google\":{\"count\":$totalCount}
 }"
+
+set -l BACKUP $DIR/timew-google-backup-(date -I).tar.gz
+
+tar -czvf $BACKUP $DIR/google
