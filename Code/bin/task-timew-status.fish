c785248f3924db4a608ba26bf49641e0ec9bb5e4
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2023-07-27 12:53:34 -0700

    misc: more scripts for tracking times

--- Code/bin/task-timew-status.fish
+++ Code/bin/task-timew-status.fish
@@ -2,7 +2,11 @@
 if timew &> /dev/null
   set -l tag (timew export @1 | jq -r '.[0].tags[0]')
   set -l timer (timew | tail -n 1 | string match -r '(\d?\d:\d\d):\d\d$')
-  echo "☑ $tag $timer[2]"
+  set -l taskout (task +ACTIVE export | jq -r '.[0].description')
+  if [ $taskout = null ]
+    $taskout = ""
+  end
+  echo "$taskout ☑ $tag $timer[2]"
 else
   echo "☑ none"
 end
