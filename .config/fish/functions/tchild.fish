954783a1b9f59a22208e92a09c7f06b4a07706ec
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2023-09-07 08:34:12 +0000

    fish: update tchild to not hardcode props

--- .config/fish/functions/tchild.fish
+++ .config/fish/functions/tchild.fish
@@ -1,19 +1,17 @@
 function tchild -d "Add a child to a task while inheriting relevant attributes" -a parent_id -a desc
   set -l pjson (task $parent_id export | jq '.[0]')
-  set -l due (echo $pjson | jq '.due')
-  set -l priority (echo $pjson | jq '.priority')
-  set -l project (echo $pjson | jq '.project')
+  echo $pjson | jq -r 'keys[]' | read -za keys
+
+  set -l skip_keys "id" "description" "entry" "modified" "status" "uuid" "urgency" "depends"
 
   set -l args $desc
-  if [ "$due" != "null" ]
-    set args $args "due:$due"
+  for key in $keys
+    if contains $key $skip_keys
+      continue
+    end
+    set args $args "$key:"(echo $pjson | jq -r ".$key")
   end
-  if [ "$priority" != "null" ]
-    set args $args "priority:$priority"
-  end
-  if [ "$project" != "null" ]
-    set args $args "project:$project"
-  end
+
   set args $args "blocks:$parent_id"
   task add $args
 end
