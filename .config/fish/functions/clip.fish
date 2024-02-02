86b37e1e316aabe725753f1ce0a1721ffb4b3feb
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-02-02 13:33:17 -0800

    fish: update clip alias

--- .config/fish/functions/clip.fish
+++ .config/fish/functions/clip.fish
@@ -1,11 +1,11 @@
 function clip -d "copy to clipboard"
-  if [ "$OS" = "Linux" ]
+  if set -q TMUX
+    tmux loadb -
+  else if [ "$OS" = "Linux" ]
     if [ "$XDG_SESSION_TYPE" = "x11" ]
       xclip -selection clipboard
     else if [ "$XDG_SESSION_TYPE" = "wayland" ]
       wl-copy
-    else if set -q TMUX
-      tmux loadb -
     end
   else if [ "$OS" = "Darwin" ]
     pbcopy
