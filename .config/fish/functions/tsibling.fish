51c106d4ca98f774d5f9c5b99a206156eee24506
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-04-22 17:33:28 -0700

    task: remove echo

--- .config/fish/functions/tsibling.fish
+++ .config/fish/functions/tsibling.fish
@@ -2,5 +2,5 @@ function tsibling -d "New task as child of current parent" -a description
   task +ACTIVE export | jq -r '.[0].uuid' | read -l active_id
   # Find the parent task
   task +PENDING export | jq -r --arg UUID $active_id 'limit(1; .[] | select(.depends[]? == $UUID) | .id)' | read -l parent_id
-  echo "tchild $parent_id $description"
+  tchild $parent_id $description
 end
