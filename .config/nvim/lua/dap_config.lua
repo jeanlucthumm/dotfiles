020964edef4d74a2b2b6a8af7d1b782e7d665a89
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-05-30 11:24:01 -0700

    nvim: update go dap config
    works with docker setup now

--- .config/nvim/lua/dap_config.lua
+++ .config/nvim/lua/dap_config.lua
@@ -6,6 +6,12 @@ dap.adapters.lldb = {
   name = 'lldb',
 }
 
+dap.adapters.delve_docker = {
+  type = 'server',
+  host = 'localhost',
+  port = 40000,
+}
+
 dap.configurations.cpp = {
   {
     name = 'Launch',
@@ -33,5 +39,30 @@ dap.configurations.cpp = {
   },
 }
 
+-- Append a dap configuration for Go
+-- TODO: this is not working yet
+if not dap.configurations.go then
+  dap.configurations.go = {}
+end
+table.insert(dap.configurations.go,
+  {
+    type = 'delve_docker',
+    name = 'Delve Docker',
+    request = 'launch',
+    mode = 'debug',
+    substitutePath = {
+      { from = '${workspaceFolder}', to = '/usr/src/app/server' },
+    },
+    program = '${relativeFile}',
+    connect = function()
+      local host = vim.fn.input('Host [localhost]: ')
+      host = host ~= '' and host or 'localhost'
+      local port = vim.fn.input('Port [40000]: ')
+      port = port ~= '' and port or '40000'
+      return { host = host, port = port }
+    end,
+  }
+)
+
 vim.fn.sign_define('DapBreakpoint',
-  { text = '🛑', texthl = '', linehl = '', numhl = '' })
+  { text = 'X', texthl = '', linehl = '', numhl = '' })
