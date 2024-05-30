c916ea0bc13a5a596ac8e1c6f9399b51251fbd31
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-05-30 11:04:23 -0700

    fish: update alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -71,7 +71,7 @@ if status is-interactive
   alias day="timew day"
   alias acc="task end.after:today completed"
   alias fix-tmux-ssh="bass (tmux show-environment -s SSH_AUTH_SOCK)"
-  alias sgpt="OPENAI_API_KEY=(cat $CONF/openai.key) command sgpt --temperature 0"
+  alias s="OPENAI_API_KEY=(cat $CONF/openai.key) command sgpt --temperature 0.1"
 end
 
 ### ===========================================================================
