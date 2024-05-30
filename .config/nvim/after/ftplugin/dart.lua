3c431f65e526e710c5f98bbe4edf7bfbe782f84a
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-05-30 11:05:57 -0700

    nvim: flutter utility F-keys

--- .config/nvim/after/ftplugin/dart.lua
+++ .config/nvim/after/ftplugin/dart.lua
@@ -4,3 +4,6 @@ vim.diagnostic.config({ update_in_insert = true })
 nmap('tf', function()
   require'harpoon.term'.sendCommand(1, 'flutter test\n')
 end)
+
+nmap('<F5>', ':FlutterQuit<CR>:sleep 1<CR>:FlutterRun<CR>')
+nmap('<F4>', ':FlutterRestart<CR>')
