8f9ca2502ea8fc9dabd3688395f72eeeac9f7679
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-06-21 08:27:38 -0700

    nvim: flutter keybind for going to test file

--- .config/nvim/after/ftplugin/dart.lua
+++ .config/nvim/after/ftplugin/dart.lua
@@ -7,3 +7,19 @@ end)
 
 nmap('<F1>', ':FlutterQuit<CR>:sleep 1<CR>:FlutterRun<CR>')
 nmap('<F2>', ':FlutterRestart<CR>')
+
+-- Go to test file for the current source file.
+-- The path for the test file is the same as the source file one relative to `lib`,
+-- but instead in the `test` directory. For example, `lib/src/foo.dart` and `test/src/foo_test.dart`.
+local function goto_test()
+  -- Check if buffer ends is _test.dart
+  local path = vim.fn.expand('%:p:r') .. '_test.dart'
+  local test_path = path:gsub('/lib/', '/test/', 1)
+  if vim.fn.filereadable(test_path) == 1 then
+    vim.cmd('edit ' .. test_path)
+  else
+    vim.notify('No test file')
+  end
+end
+
+nmap('git', goto_test)
