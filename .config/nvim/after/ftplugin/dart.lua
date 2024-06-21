d3af36b709c52933d7190a5b2d851ef50b22e5cc
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-06-21 08:27:12 -0700

    nvim: update flutter keybinds

--- .config/nvim/after/ftplugin/dart.lua
+++ .config/nvim/after/ftplugin/dart.lua
@@ -1,9 +1,9 @@
 local nmap = require'common'.nmap
 
 vim.diagnostic.config({ update_in_insert = true })
-nmap('tf', function()
-  require'harpoon.term'.sendCommand(1, 'flutter test\n')
+nmap('tb', function()
+  require'harpoon.term'.sendCommand(1, 'dart run build_runner watch\n')
 end)
 
-nmap('<F5>', ':FlutterQuit<CR>:sleep 1<CR>:FlutterRun<CR>')
-nmap('<F4>', ':FlutterRestart<CR>')
+nmap('<F1>', ':FlutterQuit<CR>:sleep 1<CR>:FlutterRun<CR>')
+nmap('<F2>', ':FlutterRestart<CR>')
