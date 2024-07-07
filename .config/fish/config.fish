1ed2b7f737c090c0ec27fc62ffbb4f175ec08c14
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-07-07 15:25:04 -0700

    fish: switch to pls for ls replacement

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -57,7 +57,7 @@ if status is-interactive
   # Default overrides
   alias vim="nvim"
   alias cat="bat"
-  alias ls="exa"
+  alias ls="pls"
   alias docker="sudo docker"
   alias ssh="TERM=xterm-256color /usr/bin/ssh"
 
