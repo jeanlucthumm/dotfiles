0c252a1c1a6cf4b1a6b6b2e5ce27569ddb131d8d
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-07-15 17:16:47 -0700

    nix: nixify fish and other stuff

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -53,7 +53,10 @@
     # User specific packages. System wide packages are in
     # environment.systemPackages and programs.
     packages = with pkgs; [
-      # Devex
+      timewarrior #       time tracker
+      grc #               colorizes CLI output
+
+      ## Devex
       sumneko-lua-language-server
       gopls
       black
@@ -79,11 +82,14 @@
   };
 
   # Home manager config. Manages user dotfiles.
-  home-manager.users.jeanluc = {config, pkgs, ...}: {
-    # The state version is required and should stay at the version you
-    # originally installed.
-    home.stateVersion = "24.05";
-
+  home-manager.users.jeanluc = {config, pkgs, ...}:
+  let
+      isLinux = pkgs.stdenv.isLinux;
+      isDarwin = pkgs.stdenv.isDarwin;
+      isArch = builtins.pathExists "/etc/arch-release";
+      homeDir = config.home.homeDirectory;
+      configDir = config.xdg.configHome;
+  in {
     programs = {
       kitty = {
         enable = true;
@@ -103,9 +109,7 @@
           paste_actions = "quote-urls-at-prompt";
         };
       };
-      taskwarrior = let
-        configDir = "${config.xdg.configHome}/task";
-      in {
+      taskwarrior = {
         enable = true;
         dataLocation = "${config.xdg.dataHome}/task";
         extraConfig = ''
@@ -114,11 +118,101 @@
           news.version=2.6.0
 
           # Put contexts defined with `task context define` in this file
-          include ${configDir}/context.config
-          hooks.location=${configDir}/hooks
+          include ${configDir}/task/context.config
+          hooks.location=${configDir}/task/hooks
+        '';
+      };
+      fish = {
+        enable = true;
+        plugins = with pkgs.fishPlugins; [
+          { name = "fzf"; src = fzf; }
+          { name = "grc"; src = grc; }
+        ];
+        shellAbbrs = {
+          g = "git";
+          t = "task";
+          docker = "sudo docker";
+          ga = "git a";
+          gm = "git com";
+          gs = "git stat";
+          gt = "git tree";
+          gd = "git d";
+          gda = "git a && git d";
+          clear-nvim-swap = "rm -rf ~/.local/state/nvim/swap";
+          ta = "task active";
+          tr = "task ready";
+          tdesc = "tprop description";
+          day = "timew day";
+          acc = "task end.after:today completed";
+        };
+        # Like shellAbbrs but doesn't auto expand when typing
+        shellAliases = {
+          vim = "nvim";
+          cat = "bat";
+          ls = "pls";
+          ssh = "TERM=xterm-256color /usr/bin/ssh";
+        };
+        interactiveShellInit = ''
+          # Jump around words easier
+          bind \ch backward-word
+          bind \cl forward-word
+
+          # Theme
+          set -g theme_nerd_fonts yes
+          set -g theme_color_scheme dark
+          fish_config theme choose Lava
+
+          if is_ssh_session; and not set -q TMUX
+            exec tmux attach
+          end
         '';
       };
     };
+
+    home = {
+      sessionVariables = {
+        EDITOR = "${pkgs.neovim}/bin/nvim";
+        MANPAGER = "sh -c 'sed -e s/.\\\\x08//g | bat -l man -p'";
+        CONF = configDir;
+        CODE = "${homeDir}/Code";
+        # Shell prompts tend to manage venvs themselves
+        VIRTUAL_ENV_DISABLE_PROMPT = 1;
+      } // (if isLinux then {
+        ANDROID_SDK_ROOT = "${homeDir}/Android/Sdk";
+        ANDROID_HOME = config.home.sessionVariables.ANDROID_SDK_ROOT;
+        CHROME_EXECUTABLE = "${pkgs.ungoogled-chromium}";
+      } else if isDarwin then {
+        ANDROID_HOME = "/Users/${config.home.username}/Library/Android/sdk";
+      } else {});
+
+      # Extra stuff to add to $PATH
+      sessionPath = if isDarwin then [
+        # homebrew puts all its stuff in this directory instead
+        # of /usr/bin or otherwise
+        "/opt/homebrew/bin"
+      ] else [];
+
+      preferXdgDirectories = true;
+    };
+
+    xdg = {
+      enable = true;
+
+      userDirs = {
+        enable = true;
+        createDirectories = true;
+        pictures = "${homeDir}/media";
+        music = "${homeDir}/media";
+        videos = "${homeDir}/media";
+        desktop = null;
+        publicShare = null;
+        templates = null;
+      };
+    };
+
+    # The state version is required and should stay at the version you
+    # originally installed.
+    home.stateVersion = "24.05";
   };
 
   # Allow unfree packages
@@ -147,7 +241,7 @@
     jq #            CLI for json manipulation
     python3 #        The language python
 
-    # Desktop
+    ## Desktop
     gammastep #     Redshifting at night
     cinnamon.nemo # File browser
     mako #          Notifications
@@ -161,31 +255,39 @@
     slurp #         For selecting screen regions
     pavucontrol #   GUI for PiperWire
     wev #           Shows keycodes in wayland
+    ungoogled-chromium  # Only used for Flutter dev
 
-    # Devex
+    ## Devex
     go #            The language Go
   ];
 
   # Programs with more config than systemPackages
   programs = {
-    hyprland.enable = true;
-    hyprlock.enable = true;
-    waybar.enable = true;
     fish.enable = true;
-    firefox = {
-      enable = true;
-      package = pkgs.firefox-bin;
-    };
+
+    # Manages GPG keys for signing stuff like git commits
     gnupg.agent = {
       enable = true;
+      # Use the CLI to provide key passwords
       pinentryPackage = pkgs.pinentry-tty;
       settings = {
+        # Don't ask for password within given time
         default-cache-ttl = 14400;
         max-cache-ttl = 14400;
       };
     };
+    # Manages SSH keys so you can do `ssh-add`
     ssh.startAgent = true;
-    seahorse.enable = true;
+
+    ## Desktop
+    firefox = {
+      enable = true;
+      package = pkgs.firefox-bin;
+    };
+    hyprland.enable = true; #     Window manager
+    hyprlock.enable = true; #     Lockscreen
+    waybar.enable = true; #       Bottom bar
+    seahorse.enable = true; #     GUI for gnome-keyring
   };
 
   # Fonts
@@ -201,7 +303,7 @@
 
   # Services
   services = {
-    # Manages ssh and gpg keys. Enables ssh-add.
+    # Manages program secrets.
     gnome.gnome-keyring.enable = true;
 
     # Audio management. Modern version of PulseAudio.
@@ -213,7 +315,15 @@
     };
   };
 
-  security.pam.services.login.enableGnomeKeyring = true;
+  security = {
+    # Unlock gnome-keyring on tty login
+    pam.services.login.enableGnomeKeyring = true;
+
+    sudo = {
+      execWheelOnly = true;
+      wheelNeedsPassword = false;
+    };
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
