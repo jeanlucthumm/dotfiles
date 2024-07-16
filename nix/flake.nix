c9c137237d109e0f1799aea393fe9fefcaa21e7b
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-07-15 19:13:35 -0700

    nix: switch to flakes for nixos

---
+++ nix/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    nixpkgs,
+    home-manager,
+    ...
+  }: {
+    nixosConfigurations."laptop" = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.jeanluc = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
