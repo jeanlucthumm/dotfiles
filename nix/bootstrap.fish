92b99727707feb64ac43599e07c1f8c78c09341a
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-07-30 20:32:55 -0700

    nix: darwin in bootstrap script

--- nix/bootstrap.fish
+++ nix/bootstrap.fish
@@ -1,6 +1,36 @@
 #!/usr/bin/env nix-shell
-#! nix-shell -i bash --pure
-#! nix-shell -p bash git
+#! nix-shell -i bash -p bash git
+
+# Check if a hostname was provided
+if test (count $argv) -eq 0
+    echo "Error: Please provide a hostname."
+    echo "Usage: ./bootstrap.fish <hostname>"
+    exit 1
+end
+
+set hostname $argv[1]
+
+function is_nixos
+    test -f /etc/NIXOS
+end
+
+function is_darwin
+    test (uname) = "Darwin"
+end
 
 git clone https://github.com/jeanlucthumm/dotfiles /tmp/dotfiles
-nixos-rebuild switch --flake /tmp/dotfiles\#laptop
+
+# Determine the OS and run appropriate commands
+if is_nixos
+    echo "NixOS detected. Building configuration for $hostname..."
+    sudo nixos-rebuild switch --flake /tmp/dotfiles/nix#$hostname
+else if is_macos
+    echo "macOS detected. Building configuration for $hostname..."
+    nix run nix-darwin \
+      --experimental-feature nix-command \
+      --experimental-feature flakes \
+      -- switch --flake /tmp/dotfiles/nix#$hostname
+else
+    echo "Error: Unsupported operating system."
+    exit 1
+end
