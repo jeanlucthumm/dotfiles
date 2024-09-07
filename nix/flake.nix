d9589fa956438fef102b0eb5f967b0c74cd16b12
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-09-06 18:22:42 -0700

    nix: dev shell

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,6 +1,8 @@
 {
-  description = "NixOS configuration";
+  description = "Jean-Luc Thumm normal systems configuration";
 
+  # Default to pinning nixpkgs to one version to avoid evaluating multiple
+  # versions.
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
@@ -11,8 +13,10 @@
       url = "github:danth/stylix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    darwin.url = "github:lnl7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
@@ -21,7 +25,18 @@
     stylix,
     darwin,
     ...
-  }: {
+  } @ inputs: let
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    # This is a function that generates an attribute by calling a function you
+    # pass to it, with each system as an argument
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
     nixosConfigurations."laptop" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -56,5 +71,24 @@
         }
       ];
     };
+
+    # Development environment to work on this flake.
+    # Since the format is `devShells.<system>.<shell_name>`, we generate the same
+    # attrs for each default system.
+    devShells = forAllSystems (
+      system:
+      # Recommended way to access nixpkgs for a specific system.
+      # Otherwise need to call nixpkgs with a specific system param.
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            statix
+            alejandra
+          ];
+        };
+      }
+    );
   };
 }
