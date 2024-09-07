91c75b268ddad1cf883649f9ce0e8342e1b5039f
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-09-07 14:41:32 -0700

    nix: add missing nix files

---
+++ nix/hosts/macbook/configuration.nix
@@ -0,0 +1,28 @@
+{pkgs, ...}: {
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # System level fish config so that we get access to nix commands
+  programs = {
+    fish.enable = true;
+    gnupg = {
+      agent = {
+        enable = true;
+        enableSSHSupport = true;
+      };
+    };
+  };
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  nixpkgs.hostPlatform = "aarch64-darwin";
+
+  users.users.jeanluc = {
+    name = "jeanluc";
+    home = "/Users/jeanluc";
+  };
+}
