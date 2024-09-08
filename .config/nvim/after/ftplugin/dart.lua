3d6bcffeccc536edb8fba8d3774b417090a3a3e1
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-09-08 11:02:30 -0700

    nvim: update dart ft plugin

--- .config/nvim/after/ftplugin/dart.lua
+++ .config/nvim/after/ftplugin/dart.lua
@@ -1,6 +1,5 @@
 local nmap = require'common'.nmap
 
-vim.diagnostic.config({ update_in_insert = true })
 nmap('tb', function()
   require'harpoon.term'.sendCommand(2, 'dart run build_runner watch\n')
 end)
