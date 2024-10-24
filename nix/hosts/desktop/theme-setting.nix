e4b728ae612f4caa5371e90194fe875acd7727df
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2024-10-24 04:25:49 -0700

    nix: externalize dark mode theme settings outside of VCS

--- nix/hosts/desktop/theme-setting.nix
+++ nix/hosts/desktop/theme-setting.nix
@@ -2,10 +2,17 @@
   imports = [
     ../../theme.nix
   ];
-  theme = {
+  theme = let
+    # Externalizes some theme settings outside of VCS.
+    # Helpful when they change frequently.
+    localSettings =
+      if builtins.pathExists ./theme-setting-local.nix
+      then import ./theme-setting-local.nix
+      else {darkMode = true;};
+  in {
     enable = true;
+    inherit (localSettings) darkMode;
     name = "gruvbox";
-    darkMode = true;
     fontCoding = {
       name = "JetBrainsMono Nerd Font Mono";
       size = 10;
