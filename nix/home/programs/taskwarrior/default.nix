c0e965e5619a118eba74693dacd069a4deefacce
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2025-04-26 11:46:47 -0700

    task: generate context in nix config

--- nix/home/programs/taskwarrior/default.nix
+++ nix/home/programs/taskwarrior/default.nix
@@ -4,26 +4,39 @@
   ...
 }: let
   configDir = config.xdg.configHome;
+  contextLabels = [
+    "cora"
+    "cora-validation"
+    "nix"
+    "chore"
+  ];
+  makeContext = label: let
+    projLabel = builtins.replaceStrings ["-"] ["."] label;
+  in ''
+    context.${label}.read=proj:${projLabel}
+    context.${label}.write=proj:${projLabel}
+  '';
 in {
   # Taskwarrior is a cli task tracking tool.
   programs.taskwarrior = {
     enable = true;
     dataLocation = "${config.home.homeDirectory}/Sync/taskwarrior";
-    extraConfig = ''
-      uda.blocks.type=string
-      uda.blocks.label=Blocks
-      uda.ticket.type=string
-      uda.ticket.label=Ticket
-      news.version=3.4.1
+    extraConfig =
+      ''
+        uda.blocks.type=string
+        uda.blocks.label=Blocks
+        uda.ticket.type=string
+        uda.ticket.label=Ticket
+        news.version=3.4.1
 
-      urgency.uda.priority.H.coefficient=6.0
-      urgency.uda.priority.M.coefficient=3.9
-      urgency.uda.priority.L.coefficient=-1.8
+        urgency.uda.priority.H.coefficient=6.0
+        urgency.uda.priority.M.coefficient=3.9
+        urgency.uda.priority.L.coefficient=-1.8
 
-      # Put contexts defined with `task context define` in this file
-      include ${configDir}/task/context.config
-      hooks.location=${configDir}/task/hooks
-    '';
+        hooks.location=${configDir}/task/hooks
+
+      ''
+      + builtins.concatStringsSep "\n" (builtins.map makeContext contextLabels);
     package = pkgs.taskwarrior3;
   };
 }
