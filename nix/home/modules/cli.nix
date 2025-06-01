6d739a382ef92be46588cc4f0eec53ab2a10ddff
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2025-05-31 20:09:03 -0700

    hypr: increase idle timeout

--- nix/home/modules/cli.nix
+++ nix/home/modules/cli.nix
@@ -16,7 +16,7 @@ in {
   home.packages = with pkgs; [
     statix # Nix linter
     alejandra # Nix formatter
-    nil # Nix LSP
+    nixd # Nix LSP
     ripgrep # Fast grep search tool
     nodejs_22 # A bunch of tools (including Copilot) rely on this
     sumneko-lua-language-server # Lua language server
