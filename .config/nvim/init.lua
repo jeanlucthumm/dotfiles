b506a619014a8b3b94bdaaac1a7368fe9653269a
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2025-06-06 11:37:36 -0700

    nvim: avante switch to claude

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -274,16 +274,11 @@ local plugin_spec = {
     event = 'VeryLazy',
     version = false, -- Never set this value to "*"! Never!
     opts = {
-      -- add any opts here
-      -- for example
-      provider = 'openai',
-      openai = {
-        endpoint = 'https://api.openai.com/v1',
-        model = 'gpt-4o',             -- your desired model (or use gpt-4o, etc.)
-        timeout = 30000,              -- Timeout in milliseconds, increase this for reasoning models
-        temperature = 0,
-        max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-        --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+      provider = 'claude',
+      providers = {
+        claude = {
+          model = 'claude-sonnet-4-20250514'
+        },
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
