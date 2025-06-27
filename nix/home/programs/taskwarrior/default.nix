1be69453ead7a1706b9217e7e28feddc6d8cc840
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2025-06-27 09:49:04 -0700

    task: modernize config

--- nix/home/programs/taskwarrior/default.nix
+++ nix/home/programs/taskwarrior/default.nix
@@ -1,8 +1,14 @@
+# Taskwarrior is a cli task tracking tool.
 {
   config,
   pkgs,
+  lib,
   ...
 }: let
+  # Contexts in taskwarrior are predefined filters that can be enabled
+  # for listing and creating tasks. We define them as a list of labels
+  # (allows for `task context <label>`), and then derive the entry in
+  # `context` attrs for it.
   contextLabels = [
     "cora"
     "cora-validation"
@@ -10,18 +16,22 @@
     "nix"
     "chore"
   ];
-  makeContext = label: let
-    projLabel = builtins.replaceStrings ["-"] ["."] label;
-  in ''
-    context.${label}.read=proj:${projLabel}
-    context.${label}.write=proj:${projLabel}
-  '';
+  makeContextEntry = label: let
+    # Proj name allows for nesting ('.'), but the label does not.
+    projName = builtins.replaceStrings ["-"] ["."] label;
+  in {
+    ${label} = {
+      read = "proj:${projName}";
+      write = "proj:${projName}";
+    };
+  };
 in {
-  # Taskwarrior is a cli task tracking tool.
   programs.taskwarrior = {
     enable = true;
-    dataLocation = "${config.xdg.dataHome}/task";
+    dataLocation = config.xdg.dataHome + "/task";
     config = {
+      # Sync to GCP. Solutions like Syncthing don't work because its a
+      # sqlite DB.
       sync = {
         gcp = {
           bucket = "taskwarrior-23423478";
@@ -29,23 +39,31 @@ in {
         };
         encryption_secret = "not-required";
       };
+      # User defined attributes (UDA) are used to add custom fields to tasks.
+      uda = {
+        # Reverse of `dep:`
+        blocks = {
+          type = "string";
+          label = "Blocks";
+        };
+        # Ties tasks to ticket/bug tracking system like Notion or Jira.
+        ticket = {
+          type = "string";
+          label = "Ticket";
+        };
+      };
+      news.version = "3.4.1";
+      # L isn't low enough by default. The rest are default values.
+      usrgency.uda.priority = {
+        H.coefficient = 6.0;
+        M.coefficient = 3.9;
+        L.coefficient = -1.8;
+      };
+      hooks.location = config.xdg.configHome + "/task/hooks";
+      # Map labels to attrs and then merge them up into one thing to assign
+      # to `context`.
+      context = lib.mergeAttrsList (lib.lists.map makeContextEntry contextLabels);
     };
-    extraConfig =
-      ''
-        uda.blocks.type=string
-        uda.blocks.label=Blocks
-        uda.ticket.type=string
-        uda.ticket.label=Ticket
-        news.version=3.4.1
-
-        urgency.uda.priority.H.coefficient=6.0
-        urgency.uda.priority.M.coefficient=3.9
-        urgency.uda.priority.L.coefficient=-1.8
-
-        hooks.location=${config.xdg.configHome}/task/hooks
-
-      ''
-      + builtins.concatStringsSep "\n" (builtins.map makeContext contextLabels);
     package = pkgs.taskwarrior3;
   };
 }
