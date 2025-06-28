ab5a944e04ec114a95d3c96347173207c10a55b0
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2025-06-28 12:29:19 -0700

    nix: simplfiy nixos foundation

--- nix/home/modules/nixos/foundation.nix
+++ nix/home/modules/nixos/foundation.nix
@@ -1,23 +1,10 @@
 # Foundational settings for NixOS.
-{
-  pkgs,
-  hostName,
-  ...
-}: {
-  home.packages = with pkgs; [
-    appimage-run # Allows for running .AppImage
-  ];
-
+{hostName, ...}: {
   programs = {
-    fish.shellAliases.nrs = "sudo nixos-rebuild switch --flake $HOME/nix#${hostName}";
     nushell.configFile.text = ''
       def nrs []: [nothing -> nothing] {
           sudo nixos-rebuild switch --flake $"($env.HOME)/nix#${hostName}"
       }
     '';
-  };
-
-  home.sessionVariables = {
-    OS = "Linux";
   };
 }
