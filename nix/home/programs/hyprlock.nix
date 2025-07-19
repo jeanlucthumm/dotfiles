c209a4cdd7f3503e31ce8d9e0327f30a54b57664
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2025-07-19 12:10:05 -0700

    hyprlock: fixup config

--- nix/home/programs/hyprlock.nix
+++ nix/home/programs/hyprlock.nix
@@ -1,28 +1,44 @@
-{config, ...}:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 with config.lib.stylix.colors; let
-  monitor = "Shenzhen KTC Technology Group - M32P10";
+  primary = with config.monitors.primary; "${manufacturer} - ${model}";
 in {
+  home.packages = with pkgs; [
+    nerd-fonts.geist-mono
+  ];
+
   programs.hyprlock = {
     enable = true;
     settings = {
-      background = [
-        {
-          monitor = monitor;
-          path = "${config.home.homeDirectory}/media/gruvbox_cat.png";
-          blur_size = 4;
-          blur_passes = 3;
-          noise = 0.0117;
-          contrast = 1.3;
-          brightness = 0.8;
-          vibrancy = 0.21;
-          vibrancy_darkness = 0.0;
-        }
-      ];
+      background =
+        [
+          {
+            monitor = primary;
+            path = "${config.home.homeDirectory}/media/gruvbox_cat.png";
+            blur_size = 4;
+            blur_passes = 3;
+            noise = 0.0117;
+            contrast = 1.3;
+            brightness = 0.8;
+            vibrancy = 0.21;
+            vibrancy_darkness = 0.0;
+          }
+        ]
+        ++ lib.optionals (config.monitors.secondary != null) [
+          {
+            monitor = with config.monitors.secondary; "${manufacturer} - ${model}";
+            color = "rgb(000000)";
+          }
+        ];
 
       label = [
         # Hours
         {
-          monitor = monitor;
+          monitor = primary;
           text = ''cmd[update:1000] echo "<b><big> $(date +"%H") </big></b>"'';
           color = "rgb(${base0C})";
           font_size = 112;
@@ -35,7 +51,7 @@ in {
         }
         # Minutes
         {
-          monitor = monitor;
+          monitor = primary;
           text = ''cmd[update:1000] echo "<b><big> $(date +"%M") </big></b>"'';
           color = "rgb(${base0C})";
           font_size = 112;
@@ -48,29 +64,29 @@ in {
         }
         # Today
         {
-          monitor = monitor;
+          monitor = primary;
           text = ''cmd[update:18000000] echo "<b><big> "$(date +'%A')" </big></b>"'';
           color = "rgb(${base05})";
           font_size = 22;
           font_family = "JetBrainsMono Nerd Font 10";
-          position = "0, 30";
+          position = "0, -20";
           halign = "center";
           valign = "center";
         }
         # Week
         {
-          monitor = monitor;
+          monitor = primary;
           text = ''cmd[update:18000000] echo "<b> "$(date +'%d %b')" </b>"'';
           color = "rgb(${base05})";
           font_size = 18;
           font_family = "JetBrainsMono Nerd Font 10";
-          position = "0, 6";
+          position = "0, -60";
           halign = "center";
           valign = "center";
         }
         # Weather
         {
-          monitor = monitor;
+          monitor = primary;
           text = ''cmd[update:18000000] echo "<b>Feels like<big> $(curl -s 'wttr.in?format=%t' | tr -d '+') </big></b>"'';
           color = "rgb(${base05})";
           font_size = 18;
@@ -83,7 +99,7 @@ in {
 
       input-field = [
         {
-          monitor = monitor;
+          monitor = primary;
           size = "250, 50";
           outline_thickness = 3;
           dots_size = 0.26;
