d6707fa8033e9555f85460dd68c3ea259090b9b0
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2025-08-03 09:31:24 -0700

    nvim: remove avante

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -282,42 +282,11 @@ local plugin_spec = {
     end,
   },
   {
-    'yetone/avante.nvim',
-    event = 'VeryLazy',
-    version = false, -- Never set this value to "*"! Never!
+    'MeanderingProgrammer/render-markdown.nvim',
     opts = {
-      provider = 'claude',
-      providers = {
-        claude = {
-          model = 'claude-sonnet-4-20250514',
-          api_key_name = { 'get-key-anthropic' },
-        },
-      },
+      file_types = { 'markdown' },
     },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = 'make',
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'stevearc/dressing.nvim',
-      'nvim-lua/plenary.nvim',
-      'MunifTanjim/nui.nvim',
-      --- The below dependencies are optional,
-      'echasnovski/mini.pick',         -- for file_selector provider mini.pick
-      'nvim-telescope/telescope.nvim', -- for file_selector provider telescope
-      'hrsh7th/nvim-cmp',              -- autocompletion for avante commands and mentions
-      'ibhagwan/fzf-lua',              -- for file_selector provider fzf
-      'nvim-tree/nvim-web-devicons',   -- or echasnovski/mini.icons
-      'zbirenbaum/copilot.lua',        -- for providers='copilot'
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { 'markdown', 'Avante' },
-        },
-        ft = { 'markdown', 'Avante' },
-      },
-    },
+    ft = { 'markdown' },
   },
   { 'mhinz/vim-signify' }, -- TODO look into nvim version
   {
