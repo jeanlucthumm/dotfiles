8927fb3534197461ea67d33af20cba9bf200c989
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2025-11-21 18:39:42 -0800

    nix: copy mini disko

---
+++ nix/system/hosts/server-mini/disko-config.nix
@@ -0,0 +1,38 @@
+{
+  # Disko configuration for eMMC-based home server
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = "/dev/mmcblk0";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "1G";
+              type = "EF00"; # EFI System Partition
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                  "umask=0077" # More secure: only root can access
+                ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+                mountOptions = ["defaults"];
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
