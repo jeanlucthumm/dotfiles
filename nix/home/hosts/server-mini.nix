f91c64cabd7b1e0d20c571f31a8b4103ec0b3505
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2026-01-15 22:25:45 +0200

    server-mini: add smartmontools

--- nix/home/hosts/server-mini.nix
+++ nix/home/hosts/server-mini.nix
@@ -5,6 +5,11 @@
     ../modules/cli/sysadmin.nix
     ../modules/ssh.nix
   ];
+
+  home.packages = with pkgs; [
+    smartmontools
+  ];
+
   # The state version is required and should stay at the version you
   # originally installed.
   home.stateVersion = "24.05";
