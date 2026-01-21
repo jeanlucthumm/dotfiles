367dec19568c0e04764653ccd6c7829c4128176b
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2026-01-21 08:26:45 -0800

    nix: remove build failure

--- nix/home/hosts/desktop.nix
+++ nix/home/hosts/desktop.nix
@@ -1,4 +1,4 @@
-{lib, ...}: {
+{...}: {
   imports = [
     ./desktop_monitors.nix
     ../modules/nixos/foundation.nix
@@ -18,8 +18,4 @@
   # The state version is required and should stay at the version you
   # originally installed.
   home.stateVersion = "24.05";
-
-  # TODO remove this once build failure is resolved.
-  # https://github.com/NixOS/nixpkgs/issues/418689
-  programs.qutebrowser.enable = lib.mkForce false;
 }
