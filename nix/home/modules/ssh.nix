5ff5ecb6b99804b06b118e8aeedee628ac29792d
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2026-02-08 16:09:44 -0800

    ssh: add connection multiplexing for server

--- nix/home/modules/ssh.nix
+++ nix/home/modules/ssh.nix
@@ -3,5 +3,12 @@
 {lib, ...}: {
   programs = {
     nushell.environmentVariables.SSH_AUTH_SOCK = lib.hm.nushell.mkNushellInline ''$"($env.XDG_RUNTIME_DIR)/ssh-agent"'';
+
+    ssh.matchBlocks.server = {
+      host = "server server.lan";
+      controlMaster = "auto";
+      controlPath = "~/.ssh/sockets/%r@%h-%p";
+      controlPersist = "4h";
+    };
   };
 }
