6459c7a3a5521a7d0454cb8f0204cff5b47853d4
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2026-03-02 19:15:31 -0800

    darwin: auto-load FIDO2 keys and set system-wide SSH_AUTH_SOCK

--- nix/home/modules/darwin/security.nix
+++ nix/home/modules/darwin/security.nix
@@ -8,4 +8,26 @@
     enable = true;
     enableNushellIntegration = true;
   };
+
+  # Point system-wide SSH_AUTH_SOCK to FIDO2-capable agent and auto-load key handles.
+  # launchctl setenv makes GUI apps (not just terminals) use the Nix agent.
+  launchd.agents.ssh-fido2-setup = {
+    enable = true;
+    config = {
+      ProgramArguments = [
+        "/bin/bash"
+        "-c"
+        ''
+          SOCK="$(getconf DARWIN_USER_TEMP_DIR)ssh-agent"
+          launchctl setenv SSH_AUTH_SOCK "$SOCK"
+          while [ ! -S "$SOCK" ]; do sleep 0.5; done
+          export SSH_AUTH_SOCK="$SOCK"
+          ${pkgs.openssh}/bin/ssh-add "$HOME/.ssh/id_ed25519_sk_signing" 2>/dev/null
+          ${pkgs.openssh}/bin/ssh-add "$HOME/.ssh/id_ed25519_sk_auth" 2>/dev/null
+        ''
+      ];
+      RunAtLoad = true;
+      KeepAlive = false;
+    };
+  };
 }
