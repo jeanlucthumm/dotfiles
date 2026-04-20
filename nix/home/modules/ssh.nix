5a440288f691d0ccd6c710778673f6432f1c2efd
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2026-04-20 11:21:39 -0700

    ssh: disable home-manager default config

--- nix/home/modules/ssh.nix
+++ nix/home/modules/ssh.nix
@@ -1,6 +1,7 @@
 # Shared SSH config (imported by both NixOS and Darwin).
 # Platform-specific SSH setup lives in nixos/ssh.nix and darwin/ssh.nix.
 {...}: {
+  programs.ssh.enableDefaultConfig = false;
   programs.ssh.matchBlocks."*" = {
     controlMaster = "auto";
     controlPath = "~/.ssh/sockets/%r@%h-%p";
