1adc82971283c6a449185044687aeb850b18a04b
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2026-04-26 14:48:09 -0700

    nix: consolidate all the foundations into the new modules

--- nix/modules/cli.nix
+++ nix/modules/cli.nix
@@ -1,6 +1,6 @@
 # CLI
-{pkgs, ...}: {
-  flake.modules.nixos.cli = {
+{...}: {
+  flake.modules.nixos.cli = {pkgs, ...}: {
     # Basic sytem wide packages
     environment.systemPackages = with pkgs; [
       file # Figure out what a certain file is
@@ -12,5 +12,21 @@
       zfs # ZFS filesystem tools
       vim # Text editor
     ];
+  };
+
+  flake.modules.homeManager.cli = {
+    config,
+    pkgs,
+    lib,
+    ...
+  }: {
+    imports = with config.flake.modules; [
+      homeManager.nushell
+    ];
+
+    home.sessionVariables = {
+      EDITOR = "${pkgs.neovim}/bin/nvim";
+      NH_FLAKE = config.home.homeDirectory + "/nix";
+    };
   };
 }
--- nix/modules/nushell/default.nix
+++ nix/modules/nushell/default.nix
@@ -1,6 +1,26 @@
 # Nushell
-{pkgs, ...}: {
+{...}: {
   flake.modules.homeManager.nushell = {
+    config,
+    pkgs,
+    lib,
+    ...
+  }: {
     home.packages = [pkgs.nushell];
+
+    programs.nushell = {
+      enable = true;
+      shellAliases = {
+        vim = "nvim";
+        fg = "job unfreeze";
+      };
+      environmentVariables = {
+        GPG_TTY = lib.hm.nushell.mkNushellInline "^tty";
+      };
+      settings = {
+        completions.algorithm = "fuzzy";
+      };
+      extraConfig = builtins.readFile ./config-base.nu;
+    };
   };
 }
