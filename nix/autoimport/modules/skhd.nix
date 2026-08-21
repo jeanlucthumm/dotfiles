9e0a55fd2905ae5e77e5279c00ddfee2607247c1
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2026-08-21 10:15:51 -0700

    skhd: absolute open path, nushell shadows open

--- nix/autoimport/modules/skhd.nix
+++ nix/autoimport/modules/skhd.nix
@@ -5,10 +5,12 @@
     darwin = {
       services.skhd = {
         enable = true;
+        # Absolute path: skhd execs through $SHELL (nushell here), whose
+        # `open` builtin shadows the macOS launcher and has no -a flag.
         config = ''
-          fn - i : open -a "Slack"
-          fn - o : open -a "kitty"
-          fn - p : open -a "Google Chrome"
+          fn - i : /usr/bin/open -a "Slack"
+          fn - o : /usr/bin/open -a "kitty"
+          fn - p : /usr/bin/open -a "Google Chrome"
         '';
       };
     };
